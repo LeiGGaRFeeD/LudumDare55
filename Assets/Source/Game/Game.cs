abca78c6d68e9a883a3d7f063e1bf67856555e5b
LeiGGaRFeeD <ridefoot@gmail.com>
2024-04-13 18:48:00 +0300

    Boiler changing when it wrong

--- Assets/Source/Game/Game.cs
+++ Assets/Source/Game/Game.cs
@@ -9,6 +9,7 @@ public class Game : MonoBehaviour
 {
     [SerializeField] private string _name;
     // Start is called before the first frame update
+    [SerializeField] private GameObject[] _boilerPic;
     void Start()
     {
         
@@ -28,67 +29,73 @@ public class Game : MonoBehaviour
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "sparklescrystalfire" || _name == "sparklesfirecrystal" || _name == "firecrystalsparkles" || _name == "firesparklescrystal" || _name == "crystalfiresparkles" || _name == "crystalfiresparkles" )
+        else if (_name == "sparklescrystalfire" || _name == "sparklesfirecrystal" || _name == "firecrystalsparkles" || _name == "firesparklescrystal" || _name == "crystalfiresparkles" || _name == "crystalfiresparkles" )
         {
             PlayerPrefs.SetInt("Turtle",2);
             Debug.Log("Turtle opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "sparklescrystalfur" || _name == "sparklesfurcrystal" || _name == "fursparklescrystal" || _name == "furcrystalsparkles" || _name == "crystalfursparkles" || _name == "crystalsparklesfur" )
+        else if (_name == "sparklescrystalfur" || _name == "sparklesfurcrystal" || _name == "fursparklescrystal" || _name == "furcrystalsparkles" || _name == "crystalfursparkles" || _name == "crystalsparklesfur" )
         {
             PlayerPrefs.SetInt("Spider",2);
             Debug.Log("Spider opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "crystalfurslurry" || _name == "crystalslurryfur" || _name == "furcrystalslurry" || _name == "furslurrycrystal" || _name == "slurrycrystalfur" || _name == "slurryfurcrystal" )
+        else if (_name == "crystalfurslurry" || _name == "crystalslurryfur" || _name == "furcrystalslurry" || _name == "furslurrycrystal" || _name == "slurrycrystalfur" || _name == "slurryfurcrystal" )
         {
             PlayerPrefs.SetInt("BoboCat",2);
             Debug.Log("BoboCat opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "sparklesfurslurry" || _name == "sparklesslurryfur" || _name == "fursparklesslurry" || _name == "furslurrysparkles" || _name == "slurrysparklesfur" || _name == "slurryfursparkles" )
+        else if (_name == "sparklesfurslurry" || _name == "sparklesslurryfur" || _name == "fursparklesslurry" || _name == "furslurrysparkles" || _name == "slurrysparklesfur" || _name == "slurryfursparkles" )
         {
             PlayerPrefs.SetInt("OsmoCat",2);
             Debug.Log("OsmoCat opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "sparklesfurfire" || _name == "sparklesfirefur" || _name == "firesparklesfur" || _name == "firefursparkles" || _name == "furfiresparkles" || _name == "fursparklesfire" )
+        else if (_name == "sparklesfurfire" || _name == "sparklesfirefur" || _name == "firesparklesfur" || _name == "firefursparkles" || _name == "furfiresparkles" || _name == "fursparklesfire" )
         {
             PlayerPrefs.SetInt("NightLamp",2);
             Debug.Log("Nightlamp opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "crystalfurfire" || _name == "crystalfirefur" || _name == "firefurcrystal" || _name == "firecrystalfur" || _name == "furcrystalfire" || _name == "furfirecrystal" )
+        else if (_name == "crystalfurfire" || _name == "crystalfirefur" || _name == "firefurcrystal" || _name == "firecrystalfur" || _name == "furcrystalfire" || _name == "furfirecrystal" )
         {
             PlayerPrefs.SetInt("Fox",2);
             Debug.Log("fox opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "sparklesslurryfire" || _name == "sparklesfireslurry" || _name == "fireslurrysparkles" || _name == "firesparklesslurry" || _name == "slurryfiresparkles" || _name == "slurrysparklesfire" )
+        else if (_name == "sparklesslurryfire" || _name == "sparklesfireslurry" || _name == "fireslurrysparkles" || _name == "firesparklesslurry" || _name == "slurryfiresparkles" || _name == "slurrysparklesfire" )
         {
             PlayerPrefs.SetInt("Jellyfish",2);
             Debug.Log("OsmoCat opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "firecrystalslurry" || _name == "firesslurrycrystal" || _name == "crystalfireslurry" || _name == "crystalslurryfire" || _name == "slurryfirecrystal" || _name == "slurrycrystalfire" )
+        else if (_name == "firecrystalslurry" || _name == "firesslurrycrystal" || _name == "crystalfireslurry" || _name == "crystalslurryfire" || _name == "slurryfirecrystal" || _name == "slurrycrystalfire" )
         {
             PlayerPrefs.SetInt("Dragon",2);
             Debug.Log("Dragon opened");
             SceneManager.LoadScene("MiniGame");
         }
         
-        if (_name == "furslurryfire" || _name == "furfireslurry" || _name == "fireslurryfur" || _name == "firefurslurry" || _name == "slurryfirefur" || _name == "slurryfurfire" )
+        else if (_name == "furslurryfire" || _name == "furfireslurry" || _name == "fireslurryfur" || _name == "firefurslurry" || _name == "slurryfirefur" || _name == "slurryfurfire" )
         {
             PlayerPrefs.SetInt("Garpy",2);
             Debug.Log("Garpy opened");
             SceneManager.LoadScene("MiniGame");
+        }
+        else
+        {
+            _boilerPic[1].SetActive(true);
+            _boilerPic[0].SetActive(false);
+            DeactivateAfterDelay();
         }
 
 
@@ -97,6 +104,22 @@ public class Game : MonoBehaviour
 
     }
     
+    
+   
+    public void DeactivateAfterDelay()
+    {
+        StartCoroutine(DeactivateCoroutine());
+    }
+
+    private IEnumerator DeactivateCoroutine()
+    {
+        // Ожидаем 2 секунды
+        yield return new WaitForSeconds(2f);
+
+        // Делаем объект неактивным
+        _boilerPic[0].SetActive(true);
+        _boilerPic[1].SetActive((false));
+    }
     
     
     //buttons logic
