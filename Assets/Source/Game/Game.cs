8018a181735f936b5e04ce27dfd36c8c87eccf34
LeiGGaRFeeD <ridefoot@gmail.com>
2024-04-13 13:02:07 +0300

    Start creating main scene

---
+++ Assets/Source/Game/Game.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private string _name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void CheckCook() //Check if ingridients are right
+    {
+        if ()
+        {
+            
+        }
+    }
+    
+    //buttons logic
+    public void ingridientOne()
+    {
+        _name = _name + "";
+    }
+
+    public void ingridientTwo()
+    {
+        _name = _name + "";
+    }
+    public void ingridientThree()
+    {
+        _name = _name + "";
+    }
+
+    public void ingridientFour()
+    {
+        _name = _name + "";
+    }
+    public void ingridientFive()
+    {
+        _name = _name + "";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
