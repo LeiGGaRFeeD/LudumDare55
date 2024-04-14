4fa446903247b1ccaea61b61b8cc3b485a88074f
LeiGGaRFeeD <ridefoot@gmail.com>
2024-04-14 15:23:25 +0300

    Added text system
    Added disabling text over time
    Updated scene

--- Assets/Source/Game/VisibleText.cs
+++ Assets/Source/Game/VisibleText.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,27 +12,52 @@ public class VisibleText : MonoBehaviour
 
     [SerializeField] private string[] _textForPersonsC = { "SL", "TU", "SP", "BO", "OS", "NI", "FO", "JE", "DR", "GA" };
 
+    [SerializeField] private GameObject[] _uiText;
     [SerializeField] private Text[] _symbols;
     private string _forErr = "You already created it!";
-    
+
+    private int a;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpenText();
     }
 
     private void OpenText()
     {
-        for (int i = 0; i < 0; i++)
+        for (int i = 0; i < _keys.Length; i++)
         {
-            if (PlayerPrefs.GetInt(_keys[i])== 1)
+            if (PlayerPrefs.GetInt(_keys[i])== 1 && PlayerPrefs.GetInt(_textForPersonsC[i]) == 0)
             {
-                if (PlayerPrefs.GetInt(_textForPersonsC[i]) == 1)
-                {
-                    
-                }
+                _uiText[i].SetActive(true);
+                a = i;
+                DeactivateAfterDelay();
+            }
+            else
+            {
+            //    _uiText[i].SetActive((false));
             }
         }
+    }
+    
+    // Вызовите этот метод, чтобы деактивировать объект через 2 секунды.
+    public void DeactivateAfterDelay()
+    {
+        StartCoroutine(DeactivateCoroutine());
+    }
+
+    private IEnumerator DeactivateCoroutine()
+    {
+        // Ожидание 2 секунд.
+        yield return new WaitForSeconds(2f);
+
+        // Деактивация объекта.
+        for (int i = 0; i < _textForPersonsC.Length; i++)
+        {
+            PlayerPrefs.SetInt(_textForPersonsC[a],2);
+            _uiText[a].SetActive(false);
+        }
+      //  gameObject.SetActive(false);
     }
 
     // Update is called once per frame
